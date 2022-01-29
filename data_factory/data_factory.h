49b42949770c615bc46c5166dc1e4e52fd14f35e
Alberto Ricardo <aricardorodriguez@hotmail.com>
2022-01-29 21:55:20 +0100

    Create initial commmands handlers

--- data_factory/data_factory.h
+++ data_factory/data_factory.h
@@ -1,3 +1,24 @@
 #ifndef __AGENTS_MIDDLEWARE_DATA_FACTORY_COMMAND_FACTORY_H_
 #define __AGENTS_MIDDLEWARE_DATA_FACTORY_COMMAND_FACTORY_H_
+
+#include "data_types/data_types.h"
+#include <vector>
+
+namespace agents {
+  namespace middleware {
+    using agents::middleware::commands;
+    using InternalCommandsHandler =
+      std::tuple<StopDeviceCommand, StartAllDevices, StartDevice, ResetDevice,
+                 RequestStatusOfAllDevices, RequestCurrentStatus, StopDevice>;
+    std::vector<std::uint8_t> GetAllCommandsCodesFromDataBase();
+    ::agent_interface_CommandWithArguments GetNanoCommandDefinitionFromDataBase(
+      std::uint8_t);
+
+    InternalCommandsHandler CreateAllInternalCommands();
+
+    MessageDictionary<::agent_interface_CommandWithArguments>
+    CreateAllNanoCommands();
+
+  } // namespace middleware
+} // namespace agents
 #endif
