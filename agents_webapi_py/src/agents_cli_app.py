61a88568aecc4473ca51f5cdd1f42ab712940be6
Alberto Ricardo <aricardorodriguez@hotmail.com>
2022-03-26 00:54:02 +0100

    add mising file

---
+++ agents_webapi_py/src/agents_cli_app.py
@@ -0,0 +1,8 @@
+from agents_webapi_py.src.agents_stub import AgentStub
+
+import logging
+
+
+if __name__ == '__main__':
+    logging.info('Agents CLI python client/server init')
+    #TODO:implment command line server client
